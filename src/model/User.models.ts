757b2dc84eaef217c0055e134881ef1d97732d99
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-18 22:17:28 +0530

    add owner field to Task model, and define methods on userSchema -> generateAccessToken, generateRefreshToken and hash password whenever modified.

--- src/model/User.models.ts
+++ src/model/User.models.ts
@@ -1,6 +1,9 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export interface Task extends Document {
+  owner: User;
   title: string;
   description: string;
   status: string;
@@ -8,7 +11,13 @@ export interface Task extends Document {
   dueDate: Date;
 }
 
-const TaskSchema: Schema<Task> = new Schema({
+const taskSchema: Schema<Task> = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+
   title: {
     type: String,
     require: true,
@@ -42,7 +51,7 @@ export interface User extends Document {
   createdAt: Date;
 }
 
-const UserSchema: Schema<User> = new Schema({
+const userSchema: Schema<User> = new Schema({
   username: {
     type: String,
     require: [true, "Username is required."],
@@ -66,7 +75,7 @@ const UserSchema: Schema<User> = new Schema({
     type: String,
   },
 
-  tasks: [TaskSchema],
+  tasks: [taskSchema],
 
   createdAt: {
     type: Date,
@@ -75,8 +84,45 @@ const UserSchema: Schema<User> = new Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this.id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET!,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
 const UserModel =
   (mongoose.models.User as mongoose.Model<User>) ||
-  mongoose.model<User>("User", UserSchema);
+  mongoose.model<User>("User", userSchema);
 
 export default UserModel;
