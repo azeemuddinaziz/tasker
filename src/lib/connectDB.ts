f7e681bb5d215a0f0cb43309dbc1a115cef9a779
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-18 21:00:23 +0530

    add connectDB file to connect with the mongoDB databse.

---
+++ src/lib/connectDB.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  if (mongoose.connections[0].readyState) {
+    console.log("MongoDB already connected.");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    console.log("Successfully connected to MongoDB databse.");
+  } catch (error) {
+    console.log("Failed connecting to MongoDB database. ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
