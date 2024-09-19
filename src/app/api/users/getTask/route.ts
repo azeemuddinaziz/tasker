d560e3d53941feb9ae4c5701b2a19456e64aefc5
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-19 23:51:29 +0530

    add getTask and updateTask route.

--- src/app/api/users/getTask/route.ts
+++ src/app/api/users/getTask/route.ts
@@ -1,17 +1,41 @@
 import connectDB from "@/lib/connectDB";
+import TaskModel from "@/model/task.models";
 import UserModel from "@/model/user.models";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  await connectDB();
-  const user = JSON.parse(req.headers.get("x-user-data")!);
+  try {
+    await connectDB();
 
-  const tasks = await UserModel.findById(user._id)
-    .select("tasks")
-    .populate("tasks");
+    const userData = req.headers.get("x-user-data");
+    if (!userData) {
+      return NextResponse.json(
+        { message: "User data is missing" },
+        { status: 400 }
+      );
+    }
 
-  return NextResponse.json({
-    messaeg: "Tasks fetched successfully",
-    data: tasks,
-  });
+    const task = await TaskModel.find({ username: "hello" }); // This is just to make sure that task model is registered - else no use.ň
+
+    const user = JSON.parse(userData);
+
+    const tasks = await UserModel.findById(user._id)
+      .select("tasks")
+      .populate("tasks");
+
+    if (!tasks) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "Tasks fetched successfully",
+      data: tasks,
+    });
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return NextResponse.json(
+      { message: "Internal server error", error },
+      { status: 500 }
+    );
+  }
 };
---
+++ src/app/api/users/updateTaskStatus/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/connectDB";
+import TaskModel from "@/model/task.models";
+
+export async function PUT(request: Request) {
+  try {
+    const { taskId, newStatus } = await request.json();
+    await connectDB();
+    await TaskModel.findByIdAndUpdate(taskId, { status: newStatus });
+    return NextResponse.json(
+      { message: "Task status updated" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error updating task status" },
+      { status: 500 }
+    );
+  }
+}
