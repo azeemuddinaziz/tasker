9c9513c89a6e910f76b89b7e70fa8ef715f6e95d
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-19 23:50:24 +0530

    update middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,22 +15,45 @@ type DecodedToken = {
 
 export const middleware = (req: NextRequest) => {
   const token = req.cookies.get("accessToken")?.value;
-  if (!token) {
-    return NextResponse.redirect("/login");
+  const url = new URL(req.url);
+  const pathname = url.pathname;
+
+  if (pathname === "/login" || pathname === "/register") {
+    if (token) {
+      // User is logged in, redirect to dashboard
+      return NextResponse.redirect(`${url.origin}/dashboard`);
+    }
+  } else if (
+    pathname.startsWith("/dashboard") ||
+    pathname.startsWith("/api/")
+  ) {
+    if (!token) {
+      // User is not logged in, redirect to login page
+      return NextResponse.redirect(`${url.origin}/login`);
+    } else {
+      try {
+        const decodedToken: DecodedToken = jwtDecode(token);
+        const response = NextResponse.next();
+        response.headers.set("x-user-data", JSON.stringify(decodedToken));
+        return response;
+      } catch (error) {
+        // Token is invalid, redirect to login page
+        return NextResponse.redirect(`${url.origin}/login`);
+      }
+    }
   }
 
-  const decodedToken: DecodedToken = jwtDecode(token);
-
-  const response = NextResponse.next();
-  response.headers.set("x-user-data", JSON.stringify(decodedToken));
-  return response;
+  return NextResponse.next();
 };
 
 export const config = {
   matcher: [
+    "/login",
+    "/register",
     "/api/users/getTask",
     "/api/users/addTask",
     "/api/users/deleteTask",
     "/api/users/editTask",
+    "/dashboard",
   ],
 };
